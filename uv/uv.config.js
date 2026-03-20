8fba3078402911e1da6bd5c8d239c218357a907b
LZRcreeperReal <vexozvexoz@gmail.com>
2026-03-20 12:10:53 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,8 +1,12 @@
-'use strict';
-
-const config = {
-	bareServerUrl: 'https://uv.bare.run/',
-	// other configurations...
+/*global Ultraviolet*/ 
+self.__uv$config = { 
+  prefix: '/bare/',
+  bare: 'https://uv.bare.run/',
+  encodeUrl: Ultraviolet.codec.xor.encode, 
+  decodeUrl: Ultraviolet.codec.xor.decode, 
+  handler: '/uv/uv.handler.js',
+  client: '/uv/uv.client.js',
+  bundle: '/uv/uv.bundle.js',
+  config: '/uv/uv.config.js',
+  sw: '/uv/uv.sw.js',
 };
-
-module.exports = config;
